a7859e41bfa53564b449c395d976a2ffb84214bf
Emre Aydemir <aydemir_emre65@hotmail.com>
2020-05-13 06:10:16 +0300

    Xamarine Carousel ve SearchBar Eklendi

---
+++ VatanBilgisayarMobil/VatanBilgisayarMobil/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using VatanBilgisayarMobil.Models;
+
+namespace VatanBilgisayarMobil.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ VatanBilgisayarMobil/VatanBilgisayarMobil/Views/Anasayfa.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VatanBilgisayarMobil.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Anasayfa : ContentPage
+    {
+        public double screensize;
+        
+        public Anasayfa()
+        {
+            InitializeComponent();
+            ImageButton ImageButton = new ImageButton();
+            List<string> FırsatÜrünleriImages = new List<string>()
+            {
+                "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/2020_nisan/ensuper-mob-min.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/2020_mayis/philips-tv-kamp-7-mayis-mob-min.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/2020_mayis/fiba-mob-3-min.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/2020_mayis/super-fiyat-xr-acer-12-mayis-mob-min.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/2020_mayis/super-fiyat-airpods-11-mayis-mob-min.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/2020_mayis/super-fiyatlar-kea-12-mayis-mob-min.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/2020_mayis/uclu-ffirin-kea-mob-12-mayis-min.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/2020_mayis/yazici-banner-12-mayis-mob-min.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/2020_mayis/lenovo-mob-12-mayis-min.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/ACER_nitro5_Vatan_700x380-mobbb.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/2020_mayis/oppo-reno3-banner-mob-min.jpg",
+                "https://cdn.vatanbilgisayar.com/Upload/BANNER//yeni-tasarim/anasayfa/2020_nisan/sss-mob-min.jpg"
+            };
+            FırsatÜrünleriCarousel.ItemsSource = FırsatÜrünleriImages;
+        }
+
+        private void FırsatResmi_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
